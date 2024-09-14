15b795c285b62cc963464e2ca7703c46cf92d8f1
Broadway-Baseball <edmundwhelan13@gmail.com>
2024-09-14 00:02:57 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,38 @@ var musicals = [
     // This is the datases. 
     // Each interest is linked to categories
     // The script determines what interest a person could have in other categories that are linked to the interests the person chose
-    { musical : "A Little Night Music", category : ["Stephen Sondheim", "Hugh Wheeler", "1970s"]},
-    { musical : "Cabaret", category : ["John Kander", "Fred Ebb", "Joe Masteroff", "1960s"]},
-    { musical : "Chicago", category : ["John Kander", "Fred Ebb", "Bob Fosse", "1970s"]},
-    { musical : "Come From Away", category : ["Irene Sankoff", "David Hein", "2010s"]},
-    { musical : "Company", category : ["Stephen Sondheim", "George Furth", "1970s"]},
-    { musical : "Evita", category : ["Andrew Lloyd Webber", "Tim Rice", "1970s"]},
+    { musical : "13", category : ["Jason Robert Brown", "Dan Elish", "2000s", "English"]},
+    { musical : "1776", category : ["Sherman Edwards", "Peter Stone", "1960s", "English"]},
+    { musical : "110 in the Shade", category : ["Harvey Schmidt", "Tom Jones", "N Richard Nash", "1960s", "English"]},
+    { musical : "1600 Pennsylvania Avenue", category : ["Leonard Bernstein", "Alan Jay Lerner", "1970s", "English"]},
+    { musical : "42nd Street", category : ["Harry Warren", "Al Dubin", "Johnny Mercer", "Michael Stewart", "1930s", "English"]},
+    { musical : "The 25th Annual Putnam County Spelling Bee", category : ["William Finn", "Rachel Sheinkin", "2000s", "English"]},
+    { musical : "70 Girls 70", category : ["John Kander", "Fred Ebb", "1970s", "English"]},
+    { musical : "9 to 5", category : ["Dolly Parton", "Patricia Resnick", "2000s", "English"]},
+    { musical : "A Bronx Tale", category : ["Alan Menken", "Glenn Slater", "Chazz Palminteri", "2010s", "English"]},
+    { musical : "A Catered Affair", category : ["John Bucchino", "Harvey Fierstein", "2000s", "English"]},
+    { musical : "A Chorus Line", category : ["Marvin Hamlisch", "Edward Kleban", "1970s", "English"]},
+    { musical : "A Christmas Story", category : ["Benj Pasek", "Justin Paul", "2010s", "English"]},
+    { musical : "A Class Act", category : ["Edward Kleban", "2000s", "English"]},
+    { musical : "A Funny Thing Happened on the Way to the Forum", category : ["Stephen Sondheim", "Burt Shevelove", "Larry Gelbart", "1960s", "English"]},
+    { musical : "A Gentleman's Guide to Love and Murder", category : ["Steven Lutvak", "2010s", "English"]},
 
-    { musical : "Follies", category : ["Stephen Sondheim", "James Goldman", "1970s"]},
-    { musical : "Gypsy", category : ["Jules Styne", "Stephen Sondheim", "Arthur Laurents", "1950s"]},
-    { musical : "Hadestown", category : ["Anais Mitchell", "2010s"]},
-    { musical : "Hairspray", category : ["Marc Shaiman", "Scott Wittman","2000s"]},
-    { musical : "Hamilton", category : ["Lin-Manuel Miranda", "2010s"]},
-    { musical : "Hello Dolly", category : ["Jerry Herman", "Michael Stewart", "1960s"]},    
+    { musical : "A Little Night Music", category : ["Stephen Sondheim", "Hugh Wheeler", "1970s", "English"]},
+    { musical : "A Night with Janis Joplin", category : ["Janis Joplin", "2010s", "English"]},
+    { musical : "A Strange Loop", category : ["Michael R Jackson", "2020s", "English"]},
+
+    { musical : "Cabaret", category : ["John Kander", "Fred Ebb", "Joe Masteroff", "1960s", "English"]},
+    { musical : "Chicago", category : ["John Kander", "Fred Ebb", "Bob Fosse", "1970s", "English"]},
+    { musical : "Come From Away", category : ["Irene Sankoff", "David Hein", "2010s", "English"]},
+    { musical : "Company", category : ["Stephen Sondheim", "George Furth", "1970s", "English"]},
+    { musical : "Evita", category : ["Andrew Lloyd Webber", "Tim Rice", "1970s", "English"]},
+
+    { musical : "Follies", category : ["Stephen Sondheim", "James Goldman", "1970s", "English"]},
+    { musical : "Gypsy", category : ["Jules Styne", "Stephen Sondheim", "Arthur Laurents", "1950s", "English"]},
+    { musical : "Hadestown", category : ["Anais Mitchell", "2010s", "English"]},
+    { musical : "Hairspray", category : ["Marc Shaiman", "Scott Wittman","2000s", "English"]},
+    { musical : "Hamilton", category : ["Lin-Manuel Miranda", "2010s", "English"]},
+    { musical : "Hello Dolly", category : ["Jerry Herman", "Michael Stewart", "1960s", "English"]},    
     ];
 // Below is the function that processes the interests given
 function process(){
