33442df46e76e6e728cfefd69970dd2a77d0ade6
Broadway-Baseball <edmundwhelan13@gmail.com>
2024-09-14 09:36:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -39,13 +39,19 @@ var musicals = [
 // Below is the function that processes the interests given
 function process(){
 let recommendations = [];
-let cat1 = document.getElementById("cat1").value;
-let cat2 = document.getElementById("cat2").value;
-let cat3 = document.getElementById("cat3").value;
+//let cat1 = document.getElementById("cat1").value;
+//let cat2 = document.getElementById("cat2").value;
+//let cat3 = document.getElementById("cat3").value;
+function displayInput() {
+    // Get the value of the input field
+    var musical1 = document.getElementById("musical1").value;
+    var musical2 = document.getElementById("musical2").value;
+    var musical3 = document.getElementById("musical3").value;
+}
     let dataE = []
-       dataE.push(cat1);
-       dataE.push(cat2);
-       dataE.push(cat3);
+       dataE.push(musical1);
+       dataE.push(musical2);
+       dataE.push(musical3);
        
 // dataE is the variable that stores the values (interests selected) from the html
 var search = dataE, categories = [];
